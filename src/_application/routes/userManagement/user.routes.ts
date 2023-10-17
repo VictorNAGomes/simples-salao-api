f984a01575be54b9ae02a0c1e3a3a8e8bfeb86bb
Joaby <joabyoliveira2004@gmail.com>
2023-10-17 20:10:50 -0300

    docs: document login endpoint

--- src/_application/routes/userManagement/user.routes.ts
+++ src/_application/routes/userManagement/user.routes.ts
@@ -4,6 +4,46 @@ import { UserController } from "../../controllers/userManagement/User.controller
 export const userRoutes = (router: ExpressRouter) => {
   const userController = new UserController();
 
+  /**
+   * @swagger
+   * /login:
+   *   post:
+   *     summary: Login de usuário
+   *     description: Login utilizado para autenticar o usuário e gerar o token de acesso
+   *     tags:
+   *       - User Management
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               email:
+   *                 type: string
+   *                 format: email
+   *                 example: 'john@email.com'
+   *               password:
+   *                 type: string
+   *                 format: password
+   *                 example: '12345678'
+   *     responses:
+   *       200:
+   *         description: Login successful
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 token:
+   *                   type: string
+   *                   description: JWT access token
+   *                 message:
+   *                   type: string
+   *                   description: Mensagem de resposta
+   *       401:
+   *         description: Invalid email or password
+   */
   router.post("/login", userController.login);
 
   return router;
